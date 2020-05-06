e48d4a934be10eefa2b7842f8a639da8098f96d8
shaosheng-js <64889965+shaosheng-js@users.noreply.github.com>
2020-05-06 18:58:24 +0800

    Update 实例6改进.c

--- 实例6改进.c
+++ 实例6改进.c
@@ -1,18 +1,18 @@
 #include <REG51.H>
-char led_mod[] = {0x38,0x5B};		//LED×ÖÄ£¡°L2¡±
-void delay(unsigned int time);
+char led_mod[] = {0x38,0x5B};		//LEDå­—æ¨¡â€œL2â€
+void delays(unsigned int time);
 
 void main() {
 	char led_point = 0;
    	while (1) { 
-		P3 = 2 - led_point; 		//Êä³öLEDÎ»Âë
-		P2 = led_mod[led_point];	//Êä³ö×ÖÄ£	
-		led_point = 1 - led_point;	//Ë¢ÐÂLEDÎ»Âë
+		P3 = 2 - led_point; 		//è¾“å‡ºLEDä½ç 
+		P2 = led_mod[led_point];	//è¾“å‡ºå­—æ¨¡	
+		led_point = 1 - led_point;	//åˆ·æ–°LEDä½ç 
 		delay(10);			
     }
 }
 
-void delay(unsigned int time){ 
+void delays(unsigned int time){ 
 	unsigned int j = 0;
 	for(;time>0;time--)
 		for(j=0;j<125;j++);
